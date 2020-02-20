d84339e9bbc2c72ae1dee4e1a45235337dfdbafc
OujyMaximus <oujy93@gmail.com>
2020-02-20 20:52:19 +0100

    Haciendo lectura del main

--- Main.java
+++ Main.java
@@ -18,11 +18,46 @@ public class Main {
 			ficheroEscritura = new FileWriter(args[1]);
 			lectura = new BufferedReader(ficheroLectura);
 			escritura = new BufferedWriter(ficheroEscritura);
+			ArrayList<Integer> descripcion = new ArrayList<>();
+			ArrayList<Integer> scores = new ArrayList<>();
+			ArrayList<Book> libros = new ArrayList<>();
+			ArrayList<Library> librerias = new ArrayList<>();
+			Book libroAux;
+			int numLinea = 3, idLibreria = 0;
+			linea = lectura.readLine();
+			String [] arrayAux = linea.split(" ");
+			for(int i=0; i<arrayAux.length; i++){
+				descripcion.add(Integer.parseInt(arrayAux[i]));
+			}
+			linea = lectura.readLine();
+			arrayAux = linea.split(" ");
+			for(int i=0; i<arrayAux.length; i++){
+				scores.add(Integer.parseInt(arrayAux[i]));
+			}
+			for(int i=0; i<scores.size(); i++){
+				libroAux = new Book(i, scores.get(i));
+				libros.add(libroAux);
+			}
+
+			// for(int i=0; i<scores.size(); i++){
+			// 	System.out.println(libros.get(i).getId() + " , " + libros.get(i).getScore());
+			// }
+
 			//Esta mierda lee el fichero
+			int librosEnBiblioteca = 0;
+			Library libreriaAux;
 			while(linea != null) {
 				linea = lectura.readLine();
 				if(linea != null) {
-					System.out.println(linea);
+					arrayAux = linea.split(" ");
+					if(numLinea % 2 != 0){
+						librosEnBiblioteca = Integer.parseInt(arrayAux[0]);
+						libreriaAux = new Library(idLibreria, Integer.parseInt(arrayAux[1]), Integer.parseInt(arrayAux[2]));
+						librerias.add(libreriaAux);
+					}else{
+
+					}
+					idLibreria++;
 				}
 			}
 			//Esta otra mierda escribe
